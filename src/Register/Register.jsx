5d101ec58db5a4ea359c8d21364e7094346301b8
Aura Špejeraitė <ArSpej@gmail.com>
2022-03-02 07:37:42 +0200

    background image fixes and registratio major fixes

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -30,100 +30,95 @@ const Register = (props) => {
   let [errList, setErrlist] = useState([]);
 
 
-  function changeData(e){
-    if (e.target.id == "reg-username"){
+  function changeData(e) {
+    if (e.target.id == "reg-username") {
       setUsername(e.target.value);
     }
-    else if (e.target.id == "reg-surname"){
+    else if (e.target.id == "reg-surname") {
       setSurname(e.target.value);
     }
-    else if (e.target.id == "reg-email"){
+    else if (e.target.id == "reg-email") {
       setEmail(e.target.value);
     }
-    else if (e.target.id == "reg-country"){
+    else if (e.target.id == "reg-country") {
       setCountry(e.target.value);
     }
-    else if (e.target.id == "reg-password"){
+    else if (e.target.id == "reg-password") {
       setPassword(e.target.value);
     }
-    else if (e.target.id == "repeat-password"){
+    else if (e.target.id == "repeat-password") {
       setRepeatPass(e.target.value);
     }
   }
 
 
-  function submitRegistration(e){
+  function submitRegistration(e) {
     let tempErrs = []
     e.preventDefault();
     let regLegit = true;
-    if(password !=repeatPass){
+    if (password != repeatPass) {
       regLegit = false;
       tempErrs.push("Passwords must match!")
       setErrlist(tempErrs)
     }
-    if (regLegit){
+    if (regLegit) {
       createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
 
-        updateProfile(auth.currentUser, {
-          displayName: "Jane Q. User"
-        }).then(() => {
-          // Profile updated!
-          props.setUser(user);
-          props.setPage("main")
+          updateProfile(auth.currentUser, {
+            displayName: "Jane Q. User"
+          }).then(() => {
+            // Profile updated!
+            props.setUser(user);
+            props.setPage("main")
+            // ...
+          }).catch((error) => {
+            // An error occurred
+            // ...
+          });
+          //props.setPage("main")
           // ...
-        }).catch((error) => {
-          // An error occurred
-          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // ..
         });
-        //props.setPage("main")
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-      });
 
     }
   }
   return (
-    <div class="registerPage">
-      <form class="main-form" id="signUpForm" onSubmit={(e) => {submitRegistration(e)}}>
-        <h3 class="form-h3">Create your personal account</h3>
-        <p class="errs">
+    <form class="register-form" id="signUpForm" onSubmit={(e) => { submitRegistration(e) }}>
+      <h3>Create your personal account</h3>
+      <p class="errs">
         {errList}
-        </p>
-        <label class="main-label">Username</label>
-        <input class="main-input" type="text" id="reg-username" onChange={(e) => {changeData(e)}} required />
-        <label class="main-label">Surname</label>
-        <input class="main-input" type="text" id="reg-surname" onChange={(e) => {changeData(e)}} required />
-        <label class="main-label">Email</label>
-        <input class="main-input" type="email" id="reg-email" onChange={(e) => {changeData(e)}} required />
-        <label class="main-select-label" htmlFor="country">Country of Residence</label>
-        <select class="main-select" id="reg-country" name="countrylist" onChange={(e) => {changeData(e)}} form="countryform" required>
-          <option value="----">----</option>
-          <option value="----">----</option>
-          <option value="----">----</option>
-          <option value="----">----</option>
-        </select>
-        <label class="main-label">Password</label>
-        <input class="main-input" type="password" id="reg-password" onChange={(e) => {changeData(e)}} required />
-        <label class="main-label">Repeat Password</label>
-        <input class="main-input" type="password" id="repeat-password" onChange={(e) => {changeData(e)}} required />
-        <label class="label-bottom">
-          <input type="checkbox" name="confirm" id="agreement"required />
-          I am 13 years of age or older and agree to the terms of the
-          <a class="bottom-links" href="#">Subscriber Agreement</a> and the
-          <a class="bottom-links" href="#">Privacy Policy</a>.</label>
+      </p>
+      <input class="main-input" type="text" id="reg-username" onChange={(e) => { changeData(e) }} required />
+      <label class="main-label">Surname</label>
+      <input class="main-input" type="text" id="reg-surname" onChange={(e) => { changeData(e) }} required />
+      <label class="main-label">Email</label>
+      <input class="main-input" type="email" id="reg-email" onChange={(e) => { changeData(e) }} required />
+      <label class="main-select-label" htmlFor="country">Country of Residence</label>
+      <select class="main-select" id="reg-country" name="countrylist" onChange={(e) => { changeData(e) }} form="countryform" required>
+        <option value="----">----</option>
+        <option value="----">----</option>
+        <option value="----">----</option>
+        <option value="----">----</option>
+      </select>
+      <label class="main-label">Password</label>
+      <input class="main-input" type="password" id="reg-password" onChange={(e) => { changeData(e) }} required />
+      <label class="main-label">Repeat Password</label>
+      <input class="main-input" type="password" id="repeat-password" onChange={(e) => { changeData(e) }} required />
+      <label class="label-bottom">
+        <input type="checkbox" name="confirm" id="agreement" required />
+        I am 13 years of age or older and agree to the terms of the
+        <a class="bottom-links" href="#">Subscriber Agreement</a> and the
+        <a class="bottom-links" href="#">Privacy Policy</a>.</label>
 
-        <input type="submit" class="btn-form" id="signUpSubmit" value="Confirm" />
-      </form>
-
-
-    </div>
+      <input type="submit" class="btn-form" id="signUpSubmit" value="Confirm" />
+    </form>
   )
 }
 
