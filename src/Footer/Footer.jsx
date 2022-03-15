4e64d3e49db2709556bea7d53c40176d81edc07a
Aura Špejeraitė <ArSpej@gmail.com>
2022-03-15 13:34:53 +0200

    added topic sections in backend

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -16,28 +16,45 @@ const Footer = (props) => {
 
   const [question, setQuestion] = useState('');
   const [email, setEmail] = useState('');
-  const [other, otherS] = useState('');
 
-  function handleInput(e) {
+  function basicInput(e) {
     e.preventDefault()
+    showBasic(!basic)
     const refs = ref(database, 'Basic Questions/');
     const newPostRef = push(refs);
     set((newPostRef), {
-      User_email: document.getElementById('email').value,
-      User_question: document.getElementById('question').value,
+      User_email: document.getElementById('email_basic').value,
+      User_question: document.getElementById('question_basic').value,
     });
+    document.getElementById('email_basic').value = '';
+    document.getElementById('question_basic').value = '';
   }
-  function otherInput(e) {
+  function accountInput(e) {
     e.preventDefault()
-    const refs = ref(database, 'Other Questions/');
+    showAccount(!account)
+    const refs = ref(database, 'Account Questions/');
     const newPostRef = push(refs);
     set((newPostRef), {
-      Other: document.getElementById('other').value,
+      User_email: document.getElementById('email_account').value,
+      User_question: document.getElementById('question_account').value,
     });
+    document.getElementById('email_basic').value = '';
+    document.getElementById('question_basic').value = '';
+  }
+  function paymentInput(e) {
+    e.preventDefault()
+    showPayment(!payment)
+    const refs = ref(database, 'Payment Questions/');
+    const newPostRef = push(refs);
+    set((newPostRef), {
+      User_email: document.getElementById('email_payment').value,
+      User_question: document.getElementById('question_payment').value,
+    });
+    document.getElementById('email_basic').value = '';
+    document.getElementById('question_basic').value = '';
   }
 
-  function closeBasic(e) {
-    e.preventDefault()
+  function closeBasic() {
     showBasic(!basic)
   }
 
@@ -57,49 +74,43 @@ const Footer = (props) => {
   }
 
   return (
-    <footer>
+    <footer className='text-light'>
       <h2>FAQ! NEED HELP?</h2>
       <h4>We've got you covered</h4>
       <p>See our most frequently asked questions below</p>
       <a href='#q2'>&#10576;Go link</a>
       <button onClick={copy}>Share</button>
       <input value={url}></input>
-      <div className='d-flex flex-column mb-5'>
+      <div className='m-3'>
         <button onClick={() => showBasic(true)}>BASIC</button>
         <div className={basic ? 'd-block' : 'd-none'}>
-          <form onSubmit={(e) => handleInput(e)}>
-            <button onClick={(e) => closeBasic(e)}>&times;</button>
+        <button onClick={(e) => closeBasic(e)}>&times;</button>
+          <form onSubmit={(e) => basicInput(e)}>
             <h5>Please state your question below. We will get back to you at the earliest!</h5>
             <label htmlFor='message'>
               Question:</label>
-            <textarea onChange={e => setQuestion(e.target.value)} id='question' type='textarea' cols='45' rows='5' placeholder='basic'></textarea>
+            <textarea onChange={e => setQuestion(e.target.value)} id='question_basic' type='textarea' cols='45' rows='5' placeholder='Tell us whats wrong!..'></textarea>
             <div>
-              <input onChange={e => setEmail(e.target.value)} id='email' type="text" placeholder='Your email' />
-              <input onChange={e => otherS(e.target.value)} id='other' type="text" placeholder='Your other' />
+              <input onChange={e => setEmail(e.target.value)} id='email_basic' type="text" placeholder='Your login email' required/>
               <button disabled={question.length < 1 || email.length < 1}>Send</button>
             </div>
           </form>
         </div>
 
-<form onSubmit={(e) => otherInput(e)}>
-<input onChange={e => otherS(e.target.value)} id='other' type="text" placeholder='Your other' />
-              <button>Send</button>
-</form>
-
         <div>
           <button onClick={() => showAccount(true)}>ACCOUNT</button>
           <div className={account ? 'd-block' : 'd-none'}>
           <button onClick={() => closeAccount(true)}>&times;</button>
-          <form onSubmit={(e) => handleInput(e)}>
-            <h5>Please state your question below. We will get back to you at the earliest!</h5>
-            <label htmlFor='message'>
-              Question:</label>
-            <textarea onChange={e => setQuestion(e.target.value)} type='textarea' cols='45' rows='5' placeholder='basic'></textarea>
-            <div>
-              <input onChange={e => setEmail(e.target.value)} type="text" placeholder='Your email' />
-              <button disabled={question.length < 1 || email.length < 1} type='button'>Send</button>
-            </div>
-          </form>
+            <form onSubmit={(e) => accountInput(e)}>
+              <h5>Please state your question below. We will get back to you at the earliest!</h5>
+              <label htmlFor='message'>
+                Question:</label>
+              <textarea onChange={e => setQuestion(e.target.value)} id='question_account' type='textarea' cols='45' rows='5' placeholder='Tell us whats wrong!..'></textarea>
+              <div>
+                <input onChange={e => setEmail(e.target.value)} id='email_account' type="text" placeholder='Your login email' required/>
+                <button disabled={question.length < 1 || email.length < 1}>Send</button>
+              </div>
+            </form>
           </div>
         </div>
 
@@ -107,18 +118,19 @@ const Footer = (props) => {
           <button onClick={() => showPayment(true)}>PAYMENT</button>
           <div className={payment ? 'd-block' : 'd-none'}>
           <button onClick={() => closePayment(true)}>&times;</button>
-          <form onSubmit={(e) => handleInput(e)}>
-            <h5>Please state your question below. We will get back to you at the earliest!</h5>
-            <label htmlFor='message'>
-              Question:</label>
-            <textarea onChange={e => setQuestion(e.target.value)} type='textarea' cols='45' rows='5' placeholder='basic' ></textarea>
-            <div>
-              <input onChange={e => setEmail(e.target.value)} type="text" placeholder='Your email' />
-              <button disabled={question.length < 1 || email.length < 1}>Send</button>
-            </div>
-          </form>
+            <form onSubmit={(e) => paymentInput(e)}>
+              <h5>Please state your question below. We will get back to you at the earliest!</h5>
+              <label htmlFor='message'>
+                Question:</label>
+              <textarea onChange={e => setQuestion(e.target.value)} id='question_payment' type='textarea' cols='45' rows='5' placeholder='Tell us whats wrong!..'></textarea>
+              <div>
+                <input onChange={e => setEmail(e.target.value)} id='email_payment' type="text" placeholder='Your login email' required/>
+                <button disabled={question.length < 1 || email.length < 1}>Send</button>
+              </div>
+            </form>
           </div>
         </div>
+
       </div>
 
       <Basic />
