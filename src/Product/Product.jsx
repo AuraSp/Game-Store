688c66f6adeb93e18620c9d115f7a995b3a752f2
Ramaleu <ramunas8250@gmail.com>
2022-03-15 13:06:51 +0200

    small product changes

--- src/Product/Product.jsx
+++ src/Product/Product.jsx
@@ -3,39 +3,40 @@ import './Product.css';
 
 const Product = (props) => {
   console.log(props)
+  let product = props.productInfo
 
   return (
     <div className='productPage'>
-    <div className='prodTitle'>{props.productInfo.name}</div>
+    <div className='prodTitle'>{product.name}</div>
     <video className="prodVideo"controls>
-                      <source src={props.productInfo.trailerUrl} type="video/mp4"/>
+                      <source src={product.trailerUrl} type="video/mp4"/>
     </video>
     <div className='productImages'>
-      <img src={props.productInfo.photo2} alt="gamePhoto"></img>
-      <img src={props.productInfo.photo3} alt="gamePhoto"></img>
-      <img src={props.productInfo.photo4} alt="gamePhoto"></img>
+      <img src={product.photo2} alt="gamePhoto"></img>
+      <img src={product.photo3} alt="gamePhoto"></img>
+      <img src={product.photo4} alt="gamePhoto"></img>
     </div>
 
     <div className='productDesc'>
-      <p>{props.productInfo.about}</p>
+      <p>{product.about}</p>
     </div>
     <div className='productRequirements'>
       <h5>System requirements</h5>
       <ul>
-          <li>OS: {props.productInfo.specs.os}</li>
-          <li>CPU: {props.productInfo.specs.cpu}</li>
-          <li>GPU: {props.productInfo.specs.gpu}</li>
+          <li>OS: {product.specs.os}</li>
+          <li>CPU: {product.specs.cpu}</li>
+          <li>GPU: {product.specs.gpu}</li>
       </ul>
     </div>
     <div className='tags'>
       <h5>Tags</h5>
       <ul>
-      {props.productInfo.tags.map((tag)=>(
+      {product.tags.map((tag)=>(
         <li key={tag}>{tag}</li>
       ))}
       </ul>
     </div>
-    <div className='prodPrice'>Price: {props.productInfo.price}$ <button className='prodBuyBtn' onClick={(e)=>{props.addToCart(props.productInfo.name)}}>Add to cart</button></div>
+    <div className='prodPrice'>Price: {product.price}$ <button className='prodBuyBtn' onClick={(e)=>{props.addToCart(product.name)}}>Add to cart</button></div>
     </div>
   )
 }
