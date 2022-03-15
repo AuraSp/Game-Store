b465aeca215e0735306c34156e584c6ca7fc188c
Aura Špejeraitė <ArSpej@gmail.com>
2022-03-15 09:36:15 +0200

    fixing database for questions

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 // import { FaWeixin } from "react-icons/fa";
 // import { IoWallet } from "react-icons/io5";
 // import { RiAccountCircleFill } from "react-icons/ri";
-import { getDatabase, ref, set, push, child } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-database.js";
+import { ref, set, push } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-database.js";
 import Basic from '../Data/BasicTopic';
 import Account from '../Data/AccountTopic';
 import Payment from '../Data/PaymentTopic';
@@ -16,19 +16,29 @@ const Footer = (props) => {
 
   const [question, setQuestion] = useState('');
   const [email, setEmail] = useState('');
+  const [other, otherS] = useState('');
 
   function handleInput(e) {
     e.preventDefault()
-    const refs = ref(database, 'USERS ASKED QUESTIONS/');
+    const refs = ref(database, 'Basic Questions/');
     const newPostRef = push(refs);
     set((newPostRef), {
       User_email: document.getElementById('email').value,
       User_question: document.getElementById('question').value,
     });
   }
+  function otherInput(e) {
+    e.preventDefault()
+    const refs = ref(database, 'Other Questions/');
+    const newPostRef = push(refs);
+    set((newPostRef), {
+      Other: document.getElementById('other').value,
+    });
+  }
 
   function closeBasic(e) {
-    showBasic(false)
+    e.preventDefault()
+    showBasic(!basic)
   }
 
   function closeAccount() {
@@ -55,40 +65,49 @@ const Footer = (props) => {
       <button onClick={copy}>Share</button>
       <input value={url}></input>
       <div className='d-flex flex-column mb-5'>
-        <div>
-          <button onClick={() => showBasic(true)}>BASIC</button>
-          <form onSubmit={(e) => handleInput(e)} className={basic ? 'd-block' : 'd-none'}>
-          <button onClick={(e) => closeBasic(e)}>&times;</button>
+        <button onClick={() => showBasic(true)}>BASIC</button>
+        <div className={basic ? 'd-block' : 'd-none'}>
+          <form onSubmit={(e) => handleInput(e)}>
+            <button onClick={(e) => closeBasic(e)}>&times;</button>
             <h5>Please state your question below. We will get back to you at the earliest!</h5>
             <label htmlFor='message'>
               Question:</label>
             <textarea onChange={e => setQuestion(e.target.value)} id='question' type='textarea' cols='45' rows='5' placeholder='basic'></textarea>
             <div>
               <input onChange={e => setEmail(e.target.value)} id='email' type="text" placeholder='Your email' />
+              <input onChange={e => otherS(e.target.value)} id='other' type="text" placeholder='Your other' />
               <button disabled={question.length < 1 || email.length < 1}>Send</button>
             </div>
           </form>
         </div>
 
+<form onSubmit={(e) => otherInput(e)}>
+<input onChange={e => otherS(e.target.value)} id='other' type="text" placeholder='Your other' />
+              <button>Send</button>
+</form>
+
         <div>
           <button onClick={() => showAccount(true)}>ACCOUNT</button>
-          <form onSubmit={(e) => handleInput(e)} className={account ? 'd-block' : 'd-none'}>
-            <button onClick={() => closeAccount(true)}>&times;</button>
+          <div className={account ? 'd-block' : 'd-none'}>
+          <button onClick={() => closeAccount(true)}>&times;</button>
+          <form onSubmit={(e) => handleInput(e)}>
             <h5>Please state your question below. We will get back to you at the earliest!</h5>
             <label htmlFor='message'>
               Question:</label>
             <textarea onChange={e => setQuestion(e.target.value)} type='textarea' cols='45' rows='5' placeholder='basic'></textarea>
             <div>
               <input onChange={e => setEmail(e.target.value)} type="text" placeholder='Your email' />
-              <button disabled={question.length < 1 || email.length < 1} >Send</button>
+              <button disabled={question.length < 1 || email.length < 1} type='button'>Send</button>
             </div>
           </form>
+          </div>
         </div>
 
         <div>
           <button onClick={() => showPayment(true)}>PAYMENT</button>
-          <form onSubmit={(e) => handleInput(e)} className={payment ? 'd-block' : 'd-none'}>
-            <button onClick={() => closePayment(true)}>&times;</button>
+          <div className={payment ? 'd-block' : 'd-none'}>
+          <button onClick={() => closePayment(true)}>&times;</button>
+          <form onSubmit={(e) => handleInput(e)}>
             <h5>Please state your question below. We will get back to you at the earliest!</h5>
             <label htmlFor='message'>
               Question:</label>
@@ -98,6 +117,7 @@ const Footer = (props) => {
               <button disabled={question.length < 1 || email.length < 1}>Send</button>
             </div>
           </form>
+          </div>
         </div>
       </div>
 
