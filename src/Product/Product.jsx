235692fd9bf4e8dfc8545474320fdacc21dc3e7f
Ramaleu <ramunas8250@gmail.com>
2022-03-18 12:40:37 +0200

    individual product css changes

--- src/Product/Product.jsx
+++ src/Product/Product.jsx
@@ -7,10 +7,12 @@ const Product = (props) => {
 
   return (
     <div className='productPage'>
-    <div className='prodTitle'>{product.name}</div>
-    <video className="prodVideo"controls>
-                      <source src={product.trailerUrl} type="video/mp4"/>
-    </video>
+    <div className='prodTitle'><h2>{product.name}</h2></div>
+    <div className='prodVideoWrap'>
+      <video className="prodVideo"controls>
+                        <source src={product.trailerUrl} type="video/mp4"/>
+      </video>
+    </div>
     <div className='productImages'>
       <img src={product.photo2} alt="gamePhoto"></img>
       <img src={product.photo3} alt="gamePhoto"></img>
@@ -18,23 +20,25 @@ const Product = (props) => {
     </div>
 
     <div className='productDesc'>
-      <p>{product.about}</p>
-    </div>
-    <div className='productRequirements'>
+      <div className='prodAbout'>
+        <p>{product.about}</p>
+      </div>
+      <div className='productRequirements'>
       <h5>System requirements</h5>
       <ul>
           <li>OS: {product.specs.os}</li>
           <li>CPU: {product.specs.cpu}</li>
           <li>GPU: {product.specs.gpu}</li>
       </ul>
-    </div>
-    <div className='tags'>
-      <h5>Tags</h5>
-      <ul>
-      {product.tags.map((tag)=>(
-        <li key={tag}>{tag}</li>
-      ))}
-      </ul>
+      </div>
+      <div className='prodTags'>
+        <h5>Tags</h5>
+        <ul>
+        {product.tags.map((tag)=>(
+          <li key={tag}>{tag}</li>
+        ))}
+        </ul>
+      </div>
     </div>
     <div className='prodPrice'>Price: {product.price}$ <button className='prodBuyBtn' onClick={(e)=>{props.addToCart(product.name)}}>Add to cart</button></div>
     </div>
