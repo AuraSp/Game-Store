a6e2d118194d82010a2d9bb45c6889f0f0210736
Aura Špejeraitė <ArSpej@gmail.com>
2022-03-21 20:22:11 +0200

    support design

--- src/Components/Support/Support.jsx
+++ src/Components/Support/Support.jsx
@@ -8,33 +8,31 @@ const Support = (props) => {
     let [successText, setSuccessText] = useState('')
 
     useEffect(() => {
-        setTimeout(() => {setSuccessText("")}, 3000);
-      }, [successText])
-      
+        setTimeout(() => { setSuccessText("") }, 3000);
+    }, [successText])
+
 
     return (
 
         <main className="main">
-            <div className="name">
-                <h2>SUPPORT</h2>
-            </div>
+            <span className='supTitle'>Support</span>
 
-            {successText}
-
+            <h3>Contact us by choosing the most suitable topic for you!</h3>
             <div className="questions">
+            {successText}
                 <div className='leftSection'>
-                {
-                    SupportData.map((item, index)=>(
-                        index <= 2 &&<SupportItem dbTopic={item.dbTopic} name={item.name} q = {item.q} db={props.db} completeSubmit={setSuccessText}/>
-                    ))
-                }
+                    {
+                        SupportData.map((item, index) => (
+                            index <= 2 && <SupportItem dbTopic={item.dbTopic} name={item.name} q={item.q} db={props.db} completeSubmit={setSuccessText} />
+                        ))
+                    }
                 </div>
                 <div className='rightSection'>
-                {
-                    SupportData.map((item, index)=>(
-                        index >= 3 &&<SupportItem dbTopic={item.dbTopic} name={item.name} q = {item.q} db={props.db} completeSubmit={setSuccessText}/>
-                    ))
-                }
+                    {
+                        SupportData.map((item, index) => (
+                            index >= 3 && <SupportItem dbTopic={item.dbTopic} name={item.name} q={item.q} db={props.db} completeSubmit={setSuccessText} />
+                        ))
+                    }
                 </div>
             </div>
         </main>
